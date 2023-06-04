d2a435df8d8eae5abe49571506e7fec46d3d25c5
Josuerodrigojr <98060884+Josuerodrigojr@users.noreply.github.com>
2023-06-04 16:49:42 -0300

    Quantidade de luzes

--- javascript/questao2.js
+++ javascript/questao2.js
@@ -1,9 +1,99 @@
 let cor = 3;
+let verificacaoVizinhos = [-1,1,11,10,9,-9,-10,-11]
 
 let selecionados = [];
 
 function clicar(posicao){
   clicarEMudarPorId(posicao)
+}
+
+function celulasVivas (pintado){
+  let vizinhosVivo = 0;
+
+
+
+  for (let i = 0; i < selecionados.length; i++) {
+
+    for (let j = 0; j < verificacaoVizinhos.length; j++) {
+      if (selecionados[i]==(parseInt(pintado) + verificacaoVizinhos[j])){
+        vizinhosVivo++;
+      }
+      
+      
+    }
+    
+    
+  }
+  return vizinhosVivo;
+}
+
+function celulasMortas(posicao){
+
+}
+
+function start(){
+  let pintados = selecionados
+  let apagar = [];
+  let acender = [];
+
+
+
+  for (let i = 0; i < pintados.length; i++) {
+
+    switch (celulasVivas(pintados[i])) {
+      case 0:
+        apagar.push(pintados[i])
+        break;
+      case 1:
+        apagar.push(pintados[i]);
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      default:
+        apagar.push(pintados[i]);
+        break;
+    }
+    
+    celulasMortas(pintados[i])
+
+    // switch (celulasMortas(pintados[i])) {
+    //   case 2:
+    //     acender.push(pintados[i]);
+        
+    //     break;
+    
+    //   default:
+    //     break;
+    // }
+
+  }
+  
+
+
+  for (let i = 0; i < apagar.length; i++) {
+    clicarEMudarPorId(apagar[i])
+    
+  }
+
+  // for (let i = 0; i < acender.length; i++) {
+  //   clicarEMudarPorId(acender[i])
+    
+  // }
+  
+}
+
+function reset(){
+  console.log('foi')
+  const limite = selecionados.length
+  for (let i = 0; i < limite; i++) {
+    console.log(selecionados[0], selecionados.length)
+    clicarEMudarPorId(selecionados[0]);
+    console.log(selecionados)
+    
+  }
+  
 }
 
 
@@ -28,6 +118,6 @@ function clicarEMudarPorId(posicao) {
     selecionados.push(liId)
 
   }
-console.log(selecionados)
+
  
 }
